f8bb2d9df1ae24e2d489d7dbea6ed3744f9f97e3
강태훈 <lkj132435@naver.com>
2021-07-28 18:21:25 +0900

    commit on2

---
+++ webgicho.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+application = Flask(__name__)
+
+
+@application.route("/hello")
+def hello():
+    return "Hello, World"
+
+
+
+if __name__ == "__main__":
+    application.run(host='localhost', port=8080)
